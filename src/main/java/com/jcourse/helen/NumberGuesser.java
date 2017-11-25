40ba5bc0f79ad26c1e3e06e42d34f51301e30f6e
Helen Lazar <helen.lazar.77@gmail.com>
2017-11-25 15:56:40 +0700

    Implement Task 2 partially

---
+++ src/main/java/com/jcourse/helen/NumberGuesser.java
@@ -0,0 +1,12 @@
+package com.jcourse.helen;
+
+import java.util.Random;
+
+public class NumberGuesser {
+    public static void main(String[] args) {
+        int num = new Random().nextInt() % 100;
+        System.out.println(num);
+        //TODO(h.lazar) implement suggestion number && attemptions count here
+    }
+
+}
---
+++ src/main/java/com/jcourse/helen/NumberGuesserIO.java
@@ -0,0 +1,19 @@
+package com.jcourse.helen;
+
+import java.io.InputStreamReader;
+
+public class NumberGuesserIO {
+    public static void main(String[] args) {
+        NumberGuesser numberGuesser = new NumberGuesser();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        char buf[] = new char[2];
+        int num;
+        try {
+            inputStreamReader.read(buf, 0, 2);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        //TODO(h.lazar) find a way to convert byte/char input to integer
+//        buf.toString()
+    }
+}
